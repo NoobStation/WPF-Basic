5df0ef8fa3ca3fc0ad30c2296a1c545bca4f59b8
NoobStation <71212753+NoobStation@users.noreply.github.com>
2020-09-13 14:33:11 -0500

    Sqlite
    Creacion,Conexion e Inserción de datos a base SQLite.
    
    Se uso nugget: System.Sqlite.Core

---
+++ WpfApp_Sqlite_Creacion_DB/WpfApp_Sqlite_Creacion_DB/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfApp_Sqlite_Creacion_DB
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Crear_DB_Click(object sender, RoutedEventArgs e)
+        {
+            //todo estara separado con el fin de comprender los codigos
+            
+            //creamos la base
+            clsCrearBase.CrearBase();
+
+            //creamos nuestra tabla de prueba, recuerda validar la creacion de la tabla
+            clsCrearBase.CrearTablaPrueba();
+
+            //agregamos datos para algunas pruebas
+            clsCrearBase.AddDatos();
+        }
+    }
+}
